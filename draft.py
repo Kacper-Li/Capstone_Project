c1393cfb04d9862a81146c4cff1256ea64a5e11f
Kacper <kacperlikus@hotmail.com>
2026-02-18 13:28:11 +0000

    Successfully sent ordered deck into new round() function and shuffle, without altering original deck

--- draft.py
+++ draft.py
@@ -6,17 +6,29 @@
 
 # Main functionality:
 # - deciding first box (both cut, who gets box)
-# - Retaining cut card
-# - keeping track of deck
-# - giving players deck cards.
+# - giving players 6 deck cards alternatively.
+# - player without box cuts for cut card
 # - allowing cards to be selected, hence removing from active hand in pegging.
 # - Keep track of hand and box and cut card for scoring afterwards
 
+# Responsibility:
+# main function should track score
+# each round resets deck, hands, box and cut card
+# main calls --> round till someone wins
+# round changes the round deck (giving players cards, cut card)
+# pegging happens in round -> round tracks live score.
+#
 
-# main function should keep track of deck, cut card, round, scores and pegging progress.
+# ORDER MATTERS:
+# GAME START = MAKE DECK FRESH
+# SHUFFLE DECK
+# GIVE EACH PLAYER 6 RANDOM CARDS, ALTERNATE, DELETE FROM GAME_DECK
+# THEN CUT CARD, UPDATE+LOG+DELETE(deck will not update anymore so this doesnt strictly matter)
+
 
 # player functions need to alternate within rounds to allow player 1 then 2 to play...
 # this will probably need to become a class rather than function so that state is persistent
+
 
 # Current plan -> make dummy beginning game 1 round.
 
@@ -45,17 +57,34 @@ scoring_types = {
 }
 
 
-def main():
-    deck = list(product(card_ranks, card_suits))
-    shuffle(deck)
+def print_deck_52(deck):
     ns = 2
     ss = 15
+    print("Printing...")
     for i in range(0, len(deck), 4):
         a = f"{deck[i][0]:{ns}} of {deck[i][1]}s"
         b = f"{deck[i+1][0]:{ns}} of {deck[i+1][1]}s"
         c = f"{deck[i+2][0]:{ns}} of {deck[i+2][1]}s"
         d = f"{deck[i+3][0]:{ns}} of {deck[i+3][1]}s"
         print(f"{a:{ss}}| {b:{ss}}| {c:{ss}}| {d}")
+    print("DONE")
+
+
+def round(base_deck: list[tuple]) -> tuple:
+    round_deck = base_deck[:]
+    shuffle(round_deck)
+    # print("Round deck:")
+    # print(print_deck_52(round_deck))
+
+
+def main():
+    base_deck = list(product(card_ranks, card_suits))
+    # print("Base deck:")
+    # print_deck_52(base_deck)
+    round(base_deck)
+    # print("Base deck after round:")
+    # print_deck_52(base_deck)
+
     player_1_score = 0
     player_2_score = 0
     pegging_value = 0
