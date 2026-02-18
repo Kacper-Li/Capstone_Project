9144d7ffc92d0b18f5345c6c42f5905c17ad9cec
Kacper <kacperlikus@hotmail.com>
2026-02-18 10:05:50 +0000

    First draft for game logic

---
+++ draft.py
@@ -0,0 +1,15 @@
+# 4 main aspects
+# - pegging
+# - hand
+# - box
+# - cut card
+
+# main function should keep track of deck, cut card, round, scores and pegging progress.
+
+# player functions need to alternate within rounds to allow player 1 then 2 to play...
+# this will probably need to become a class rather than function so that state is persistent
+
+# Current plan -> make dummy beginning game 1 round.
+
+def main():
+
