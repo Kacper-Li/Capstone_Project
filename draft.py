11cab05271a3c8978652be089c42f5d90eebd5d7
Kacper <kacperlikus@hotmail.com>
2026-02-18 12:10:38 +0000

    Managed to generate and output a full deck of cards

--- draft.py
+++ draft.py
@@ -4,6 +4,15 @@
 # - box
 # - cut card
 
+# Main functionality:
+# - deciding first box (both cut, who gets box)
+# - Retaining cut card
+# - keeping track of deck
+# - giving players deck cards.
+# - allowing cards to be selected, hence removing from active hand in pegging.
+# - Keep track of hand and box and cut card for scoring afterwards
+
+
 # main function should keep track of deck, cut card, round, scores and pegging progress.
 
 # player functions need to alternate within rounds to allow player 1 then 2 to play...
@@ -11,5 +20,43 @@
 
 # Current plan -> make dummy beginning game 1 round.
 
-def main():
 
+# Make this global just cuz it will be used probably everywhere
+from itertools import product
+from random import shuffle
+
+card_suits = ['Club', 'Diamond', 'Heart', 'Spade']
+card_ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+scoring_types = {
+    'type': "score_value",
+    '15': 2,
+    'pair': 2,
+    'pair royale': 6,
+    'double pair royale': 12,
+    'run of 3': 3,
+    'run of 4': 4,
+    'run of 5': 5,
+    'flush of 4': 4,
+    'flush of 5': 5,
+    'last peg': 1,
+    '31': 2,
+    '2 for his heels': 2,
+    '1 for his nobs': 1,
+}
+
+
+def main():
+    deck = list(product(card_ranks, card_suits))
+    for i in range(0, len(deck), 4):
+        a = f"{deck[i][0]:2} of {deck[i][1]}s"
+        b = f"{deck[i+1][0]:2} of {deck[i+1][1]}s"
+        c = f"{deck[i+2][0]:2} of {deck[i+2][1]}s"
+        d = f"{deck[i+3][0]:2} of {deck[i+3][1]}s"
+        print(f"{a:12}| {b:15}| {c:13}| {d}")
+    player_1_score = 0
+    player_2_score = 0
+    pegging_value = 0
+    cut_card = ''
+
+
+main()
