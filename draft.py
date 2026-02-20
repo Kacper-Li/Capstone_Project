cc942628e8d98c57394ff1b0e4b175c6700fd0fa
Kacper <kacperlikus@hotmail.com>
2026-02-20 17:35:35 +0000

    Moved all overarching functionality to card_utils to avoid circular imports

--- draft.py
+++ draft.py
@@ -36,58 +36,15 @@
 # Make this global just cuz it will be used probably everywhere
 from itertools import product
 from random import shuffle
-import random
-
-
-Card = tuple[str, str]
-
-card_suits = ['Club', 'Diamond', 'Heart', 'Spade']
-card_ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-scoring_types = {
-    'type': "score_value",
-    '15': 2,
-    'pair': 2,
-    'pair royale': 6,
-    'double pair royale': 12,
-    'run of 3': 3,
-    'run of 4': 4,
-    'run of 5': 5,
-    'flush of 4': 4,
-    'flush of 5': 5,
-    'last peg': 1,
-    '31': 2,
-    '2 for his heels': 2,
-    '1 for his nobs': 1,
-}
-
-
-def print_deck(deck: list[Card]) -> None:
-    """Takes any list of cards and prints them out in 4 structured columns"""
-    ns = 2
-    ss = 14
-    # print("Printing...")
-    for i in range(0, len(deck), 4):
-        formatted_parts = []
-        for card in deck[i:i+4]:
-            card_string = f"{card[0]:{ns}} of {card[1]}s"
-            formatted_parts.append(f"{card_string:{ss}}")
-        line = ' | '.join(formatted_parts)
-        print(line)
-    # print("DONE")
+from card_utils import Card
+from card_utils import pick_a_card
+from card_utils import print_deck
+from card_utils import card_ranks
+from card_utils import card_suits
 
 
 # General function to get a card out of any set of cards (hand, box or deck!)
 # Needs to be changed to allow a choice rather than random.
-
-
-def pick_a_card(card_set: list[Card], index=0) -> Card:
-    """
-    Remove and return a random (rank, suit) card from any card_set.\n
-    Manipulates the list of cards directly.
-    """
-    pick_a_card = card_set.pop(random.randint(0, len(card_set) - 1))
-    # print(f"The card chosen at random: {pick_a_card[0]} of {pick_a_card[1]}s")
-    return pick_a_card
 
 
 def hands_init(
