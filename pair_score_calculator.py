160ee6426e2833bca67c49c5996967950557f84b
Kacper <kacperlikus@hotmail.com>
2026-02-21 15:22:14 +0000

    Again same thing, passing card[0] where card expected, trying to shortcut my own functions leading to logical glitches. STOP

--- pair_score_calculator.py
+++ pair_score_calculator.py
@@ -11,13 +11,13 @@ def find_biggest_pair(cards_played: list[Card]) -> str:
     # print_deck(cards_played)
     cards_relevant = cards_played[:]
 
-    if card_order(cards_relevant[-1][0]) != card_order(cards_relevant[-2][0]):
+    if card_order(cards_relevant[-1]) != card_order(cards_relevant[-2]):
         return '0'
     elif (card_no > 2):
-        if card_order(cards_relevant[-2][0]) != card_order(cards_relevant[-3][0]):
+        if card_order(cards_relevant[-2]) != card_order(cards_relevant[-3]):
             return 'pair'
         elif (card_no > 3):
-            if card_order(cards_relevant[-3][0]) != card_order(cards_relevant[-4][0]):
+            if card_order(cards_relevant[-3]) != card_order(cards_relevant[-4]):
                 return 'pair royale'
             else:
                 return 'double pair royale'
@@ -35,6 +35,8 @@ pair2 = [('4', 'Heart'), ('4', 'Club'), ('4', 'Diamond'), ('4', 'Spade')]
 # Double Pair Royale / Four of a kind (12 points)
 pair3 = [('6', 'Heart'), ('6', 'Club'), ('9', 'Diamond'), ('9', 'Spade')]
 # Two pairs with a gap (2 points)
+test0 = [('3', 'Spade'), ('10', 'Heart'), ('A', 'Heart')]
+# A logical glitch sees 10 and A as the same
 
 # Tests:
 # test = find_biggest_pair(pair0)
@@ -43,5 +45,5 @@ pair3 = [('6', 'Heart'), ('6', 'Club'), ('9', 'Diamond'), ('9', 'Spade')]
 # print(f"Found: {test}")
 # test = find_biggest_pair(pair2)
 # print(f"Found: {test}")
-# test = find_biggest_pair(pair3)
+# test = find_biggest_pair(test0)
 # print(f"Found: {test}")
