ab4b28179ef3257ca5ce75e2e6d9d9aa8fb5a09e
Kacper <kacperlikus@hotmail.com>
2026-02-21 15:31:45 +0000

    tested how to only allow playable cards to be chosen in pegging stage

--- testing.py
+++ testing.py
@@ -1,1 +1,20 @@
-print(print("hello"), "hi", "who", print("are"), "you?")
+from card_utils import *
+from pegging_functionality import *
+
+
+hand = [('2', 'Club'), ('9', 'Spade'), ('1', 'Heart'), ('Q', 'Diamond'),
+        ('3', 'Diamond'), ('J', 'Diamond')]
+card_pile = [('10', 'Club'), ('J', 'Club'), ('8', 'Heart')]
+
+
+playable_cards = can_place(hand, card_pile)
+print("Playable: ", playable_cards)
+card_to_play = random.choice(playable_cards)
+# how to specify to pick from hand something in
+print(card_to_play)
+hand.remove(card_to_play)
+card_pile.append(card_to_play)
+
+print()
+print(card_pile)
+print(hand)
