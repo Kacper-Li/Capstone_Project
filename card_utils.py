9091a83814fe6b1ab66d3efaa2579a510fdddce5
Kacper <kacperlikus@hotmail.com>
2026-02-21 17:39:50 +0000

    Added default value as a false alarm to total cards value. The problem to solve was infinite loop elsewhere but never hurts to have an extra quick check

--- card_utils.py
+++ card_utils.py
@@ -95,6 +95,8 @@ def pick_a_card(card_set: list[Card], index=0) -> Card:
 def total_cards_value(cards: list[Card]) -> int:
     """Takes in some cards, puts out their combined rank value!"""
     total_card_rank = 0
+    if cards == []:
+        return 0
     for card in cards:
         total_card_rank += card_value(card)
     return total_card_rank
