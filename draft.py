099b443a2729efd02a8a20d6adac20e4bd4cb66e
Kacper <kacperlikus@hotmail.com>
2026-02-22 10:54:03 +0000

    Massive checkpoint, made functionality all work together to fully bring together a full round of play. It uses currently tracked scores so linking rounds together should be easy peasy. This is a milestone.

--- draft.py
+++ draft.py
@@ -42,6 +42,8 @@ from card_utils import print_deck
 from card_utils import card_ranks
 from card_utils import card_suits
 from card_utils import hands_init
+from pegging_functionality import pegging_stage
+from scoring_calculation import hand_scoring, box_scoring
 
 
 # General function to get a card out of any set of cards (hand, box or deck!)
@@ -62,63 +64,106 @@ def box_init(
     print_deck(box)
     return box
 
-
-def pegging_stage(
-    pegging_pile: list[Card],
-    p1_hand: list[Card],
-    p2_hand: list[Card]
-) -> tuple[int, int]:
-    print("NOT MADE YET")
-
-
-def calculate_score(*args) -> int:
-    print("NOT MADE YET")
-
 # Shuffle deck DONE
 # Manipulate deck DONE
 # Create Hands (active/ base) DONE
 # Create box DONE
 # Get cut card DONE
-# Pegging pipeline <--
+# Pegging pipeline DONE DONE DONE
 
 
-def round(base_deck: list[Card]) -> tuple[int, int]:
-    """The main linking of functions, and game flow, to allow the overall game to be played."""
+def round_functionality(
+    base_deck: list[Card],
+    p1_score: int,
+    p2_score: int,
+    round: int
+) -> tuple[int, int]:
+    """The main linking of functions, and game flow, to allow the overall game to be played.\n
+    Returns player scores p1, p2 and controls flow of who gets box, pegging, scoring hand and box."""
     round_deck = base_deck[:]
     shuffle(round_deck)
 
     player_1_hand, player_2_hand = [], []
     hands_init(round_deck, player_1_hand, player_2_hand)
-    player_1_hand_base = player_1_hand[:]
-    player_2_hand_base = player_2_hand[:]
+    player_1_hand_copy = player_1_hand[:]
+    player_2_hand_copy = player_2_hand[:]
 
     box = box_init(player_1_hand, player_2_hand)
 
-    # print("Player 1 Hand:")
-    # print_deck(player_1_hand)
-    # print("Player 2 Hand:")
-    # print_deck(player_2_hand)
-
+    print("Player 1 Hand:")
+    print_deck(player_1_hand)
+    print("Player 2 Hand:")
+    print_deck(player_2_hand)
+    separator = "||||||||||||||||||||"
     print("Round deck:")
     print_deck(round_deck)
     cut_card = pick_a_card(round_deck)
-    print_deck([cut_card])
+    print("Cut card chosen randomly: ", cut_card)
+    print(f"{separator} ROUND BEGINS!!!! {separator}")
+    if (round % 2) == 1:
+        print("Player 1's turn with box!")
+        # meaning player 2 places first in pegging
+        p2_diff, p1_diff = pegging_stage(
+            player_2_hand_copy, player_1_hand_copy, p2_score, p1_score)
+        p1_score += p1_diff
+        p2_score += p2_diff
+        print(f"{separator} Pegging done! Moving on! {separator}")
+        print("\nSince player 1 has box, player 2 gets their hand scored first!")
+        print("Hand:")
+        p2_diff = hand_scoring(player_2_hand, cut_card)
+        p2_score += p2_diff
+        print(f"Points earned: {p2_diff}")
+        print("\nNow player 1 gets their hand and then box scored!")
+        print("Hand:")
+        p1_diff = hand_scoring(player_1_hand, cut_card)
+        p1_score += p1_diff
+        print(f"Points earned: {p1_diff}")
+        print("Box:")
+        p1_diff = box_scoring(box, cut_card)
+        p1_score += p1_diff
+        print(f"Points earned: {p1_diff}")
+        print(f"{separator} Scoring round completely finished! {separator}")
 
-    pegging_pile = []
-    pegging_stage(pegging_pile, player_1_hand, player_2_hand)
+    else:
+        print("Player 2's turn with box!")
+        # meaning player 1 places first in pegging
+        p1_diff, p2_diff = pegging_stage(
+            player_1_hand_copy, player_2_hand_copy, p1_score, p2_score)
+        p1_score += p1_diff
+        p2_score += p2_diff
+        print(f"{separator} Pegging done! Moving on! {separator}")
+        print("\nSince player 2 has box, player 1 gets their hand scored first!")
+        print("Hand:")
+        p1_diff = hand_scoring(player_1_hand, cut_card)
+        p1_score += p1_diff
+        print(f"Points earned: {p1_diff}")
+        print("\nNow player 2 gets their hand and then box scored!")
+        print("Hand:")
+        p2_diff = hand_scoring(player_2_hand, cut_card)
+        p2_score += p2_diff
+        print(f"Points earned: {p2_diff}")
+        print("Box:")
+        p2_diff = box_scoring(box, cut_card)
+        p2_score += p2_diff
+        print(f"Points earned: {p2_diff}")
+        print(f"{separator} Scoring round completely finished! {separator}")
 
-    calculate_score(player_1_hand_base, cut_card)
-    calculate_score(player_2_hand_base, cut_card)
-    calculate_score(box, cut_card)
+    print(f"Player 1: {p1_score}, Player 2: {p2_score}")
+    return p1_score, p2_score
 
 
 def main():
+    """While no winner is decided: keeps looping rounds!"""
     base_deck = list(product(card_ranks, card_suits))
     player_1_score = 0
     player_2_score = 0
+    round_number = 1
     # print("Base deck:")
     # print_deck(base_deck)
-    round(base_deck)
+    p1_diff, p2_diff = round_functionality(base_deck, player_1_score,
+                                           player_2_score, round_number)
+    player_1_score += p1_diff
+    player_2_score += p2_diff
     # print("Base deck after round:")
     # print_deck(base_deck)
 
